747c45bd28891c8c13bf4453274c3565ac6df4b3
soundaryakannan <30754796+soundaryakannan@users.noreply.github.com>
2019-04-18 18:54:43 +0530

    Add files via upload

---
+++ best1.ino
@@ -0,0 +1,54 @@
+const int trigPin = 4;
+const int echoPin = 2;
+int vibr_Pin =3;
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+void setup() 
+{ 
+   pinMode(vibr_Pin, INPUT);
+   radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+Serial.begin(9600);
+} 
+void loop()
+{
+ 
+long duration, inches, cm;
+pinMode(trigPin, OUTPUT); 
+digitalWrite(trigPin, LOW); 
+delayMicroseconds(2); 
+digitalWrite(trigPin, HIGH); 
+delayMicroseconds(10); 
+digitalWrite(trigPin, LOW);
+pinMode(echoPin, INPUT); 
+duration = pulseIn(echoPin, HIGH);
+cm = microsecondsToCentimeters(duration);
+Serial.print(cm);
+Serial.println("mm"); 
+delay(100);
+while(cm<200&&cm!=0)
+{
+  long measurement =TP_init();
+  delay(50);
+  if (measurement > 1000){
+     Serial.println("VIBED");
+     const char text[] = "ALERT";
+     radio.write(&text, sizeof(text));
+     delay(1000);
+  }
+}
+delay(1000);
+}
+
+long microsecondsToCentimeters(long microseconds)
+{return microseconds / 29 / 2;}
+long TP_init(){
+  delay(10);
+  long measurement=pulseIn (vibr_Pin, HIGH);  //wait for the pin to get HIGH and returns measurement
+  return measurement;
+}
---
+++ rec.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+int led=10;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  
+  if (radio.available())
+  {
+    
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+   
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW);
+    delay(500);
+    Serial.println(text);
+    
+  }
+}
+
+
